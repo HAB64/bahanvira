a6035118986aa512b7bf0e87f56187e8a3873279
Z User <z@container>
2026-07-06 16:47:27 +0000

    f92bb2e5-f02e-4bd0-9b07-a00c74d44569

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/bahanvira",
+  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
